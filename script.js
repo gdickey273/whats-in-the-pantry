4618420db446a2ea98f12d1a3dfb3cf1840a223f
martinhemphill <mthemphi@gmail.com>
2020-08-01 13:22:17 -0400

    completed recipe card encapsulated within recipe div

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var ingredients = prompt(); // When input grab is available change query to input
+var ingredients = "chicken, rice"; // When input grab is available change query to input
 var queryURL = "https://api.edamam.com/search?q="
 
 function updatePage(recipeData) {
@@ -11,28 +11,38 @@ function updatePage(recipeData) {
       var recipe = recipeData.hits[i];
       console.log(recipe);
 
+      var recipeDiv = $("<div>")
+      recipeDiv.addClass("recipe-card")
+      recipeDiv.appendTo("body");
+
       // Recipe Name
       var recipeName = recipeData.hits[i].recipe.label;
       console.log(recipeName);
-      $("<div>", {
-        "class": "Recipie Container",
-        "id" : "Recipie" + i,
-
-      })
+      var name = $("<p>")
+      name.html(recipeName);
+      name.appendTo(recipeDiv); 
+      
 
       // Recipe Calories
       var recipeCalories = recipeData.hits[i].recipe.calories;
       console.log(recipeCalories);
+      var calories = $("<p>")
+      calories.html(Math.round(recipeCalories));
+      calories.appendTo(recipeDiv);
 
-      // Recipe Health Labels i.e Alcohol Free
+
+      // Recipe Health Labels i.e Alcohol Free, Gluten Free
       var recipeHealthLabels = recipeData.hits[i].recipe.healthLabels;
       console.log(recipeHealthLabels);
+      var health = $("<p>")
+      health.html([recipeHealthLabels]);
+      health.appendTo(recipeDiv);
       
       // Recipe Image
       var recipeImage = recipeData.hits[i].recipe.image;
       var img = $("<img>")
       img.attr("src", recipeImage);
-      img.appendTo("body");
+      img.appendTo(recipeDiv);
   
   }
 
