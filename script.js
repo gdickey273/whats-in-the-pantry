9e40086c485f8390fe7f0639f57faa3f87676c73
gdickey273 <gdickey273@gmail.com>
2020-08-01 13:43:19 -0400

    Added parseIngredients() function

--- script.js
+++ script.js
@@ -39,6 +39,36 @@ function updatePage(recipeData) {
 }
 
 
+//Takes an array of Ingredient Lines (i.e. ["4 Cups of Chicken Broth", "2 Teaspoons of Salt"]) as an argument and returns an array with an Ingredient object for each Ingredient Lin.e
+function parseIngredients(ingLines){
+  var ingredients = [];
+  for(var i = 0; i < ingLines.length; i++){
+    $.ajax({
+      url: "https://api.spoonacular.com/recipes/parseIngredients?ingredientList="+ ingLines[i] + "&apiKey=" + spoonacularKey,
+      beforeSend: function(xhr){
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      },
+      method: "POST"
+    }).then(function(response){
+      parseIngredientOBJ = response;
+      var ingOBJ = {
+        line: response[0].original,
+        name: response[0].name,
+        id: response[0].id,
+        imgURL: "https://spoonacular.com/cdn/ingredients_100x100/" + response[0].name,
+        unit: response[0].unit,
+        amount: response[0].amount,
+        amountCost: response[0].estimatedCost.value,
+        packageCost: -1
+      }
+      ingredients.push(ingOBJ);
+    });
+  }
+  return ingredients;
+}
+
+
+
 queryURL = "https://api.edamam.com/search?q=" + ingredients + "&app_id=5af58a3f&app_key=b16b5ae107b9c5c0a2d30d43d00f64b7";
 $.ajax({
   url: queryURL,
