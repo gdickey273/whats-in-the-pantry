ae8a73d39cf7a79d0b931d471b3c94d9385bc551
gdickey273 <gdickey273@gmail.com>
2020-08-04 21:42:31 -0400

    error ridden script file

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 var ingredients = prompt(); // When input grab is available change query to input
-var edamamQueryURL = "https://api.edamam.com/search?q="
-var appId = "&app_id=5af58a3f"
-var apiKey = "&app_key=b16b5ae107b9c5c0a2d30d43d00f64b7"
+var edamamQueryURL = "https://api.edamam.com/search?q=";
+var appId = "&app_id=595f4e2b";
+var apiKey = "&app_key=d8d22c089617d4cfbff9ce15762ee548";
+var spoonacularKey = "fd6475bc93094d129e4695440a886f1a";
+var ingredientArray = [];
 
 function updatePage(recipeData) {
   console.log("-----------------------------------")
   console.log(recipeData);
   console.log("-----------------------------------")
   
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 1; i++) {
       var recipe = recipeData.hits[i];
       console.log(recipe);
 
@@ -43,12 +45,22 @@ function updatePage(recipeData) {
       img.attr("src", recipeImage);
       img.appendTo(recipeDiv);
   
+      var ingLines = recipeData.hits[i].recipe.ingredientLines;
+      
+      parseIngredients(ingLines);
+      
+    $.when(){
+      buildIngredientsList(ingredientArray);
+    }
+   
+    
+  
+      
   }
 }
 
 //Takes an array of Ingredient Lines (i.e. ["4 Cups of Chicken Broth", "2 Teaspoons of Salt"]) as an argument and returns an array with an Ingredient object for each Ingredient Lin.e
 function parseIngredients(ingLines){
-  var ingredients = [];
   for(var i = 0; i < ingLines.length; i++){
     $.ajax({
       url: "https://api.spoonacular.com/recipes/parseIngredients?ingredientList="+ ingLines[i] + "&apiKey=" + spoonacularKey,
@@ -58,6 +70,7 @@ function parseIngredients(ingLines){
       method: "POST"
     }).then(function(response){
       parseIngredientOBJ = response;
+      console.log("-----response-----", response);
       var ingOBJ = {
         line: response[0].original,
         name: response[0].name,
@@ -68,15 +81,50 @@ function parseIngredients(ingLines){
         amountCost: response[0].estimatedCost.value,
         packageCost: -1
       }
-      ingredients.push(ingOBJ);
+       ingredientArray.push(ingOBJ);
+       
     });
   }
-  return ingredients;
+  console.log("-----ingredients------", ingredientArray.length);
+  
+  
+  
 }
 
-queryURL = edamamQueryURL + ingredients + appId + apiKey;
+function buildIngredientsList(ingArray){
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(updatePage);
+  console.log("Running build ing list!");
+  var subTotal = 0;
+  var ingDiv = $("<div>");
+  var ingTable = $("<table>").attr("id", "ingredient-table");
+  ingDiv.append(ingTable);
+  ingDiv.appendTo("body");
+
+  
+  var ingTableHeadRow = $("<tr>");
+  ingTableHeadRow.append($("<th>").html("Image"));
+  ingTableHeadRow.append($("<th>").html("Ingredient Line"));
+  ingTableHeadRow.append($("<th>").html("Cost"));
+  ingTableHeadRow.append($("<th>").html("Link"));
+  ingTable.append(ingTableHeadRow);
+
+  console.log(Array.isArray(ingArray));
+  console.log(ingArray.length);
+  ingArray.forEach(function(ingredient){
+    console.log("in the loop!")
+    console.log(ingredient);
+    var ingImage = $("<td>").append($("<img>").attr(ingredient.imgURL));
+    var ingLine = $("<td>").append($("<p>").html(ingredient.line));
+    var ingCost = $("<td>").append($("<p>").html(ingredient.amountCost));
+    var ingLink = $("<td>").append($("<a>").attr("href", "https://www.amazon.com/s?k=" + ingredient.name +"&i=grocery"));
+
+    var ingRow = $("<tr>").append(ingImage, ingLine, ingCost, ingLink);
+    $("#ingredient-table").append(ingRow);
+
+    subTotal += ingredient.amountCost;
+
+
+  });
+
+  ingDiv.append($("<h4>").html("Total Recipe Cost in Cents: " + subTotal));
+}
